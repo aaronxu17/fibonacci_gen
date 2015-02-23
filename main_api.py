5987d4b8f3d7af3b862f625e6442a7fed106e858
Aaron Xu <yiranxu.usu@gmail.com>
2015-02-22 19:11:19 -0700

    v4

--- main_api.py
+++ main_api.py
@@ -2,7 +2,7 @@
 
 from flask import Flask
 from flask.ext.restful import reqparse, abort, Api, Resource
-import single_task
+from single_task import *
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,11 +14,7 @@ INDEX = {
     'task3': {'#3': 'Adding more tasks...'},
 }
 
-# Error message for no-exist task_id
-def abort_if_task_doesnt_exist(task_id):
-    if task_id not in INDEX:
-        abort(404, message="Task {} does not exist".format(task_id))
-        
+    
 parser = reqparse.RequestParser()
 parser.add_argument('task', type=str)
 
@@ -28,7 +24,7 @@ class Index(Resource):
         return INDEX
 
 api.add_resource(Index,'/')
-api.add_resource(single_task.SingleTask, '/<string:task_id>')
+api.add_resource(SingleTask, '/<string:task_id>')
 # api.add_resource(fibo_gen.FiboGen,'/index/task1/<string:Fib_n>')
 
 if __name__=='__main__':
--- single_task.py
+++ single_task.py
@@ -2,8 +2,14 @@
 from flask.ext.restful import reqparse, abort, Api, Resource
 
 class SingleTask(Resource):
+    # Error message for no-exist task_id
+    def abort_if_task_doesnt_exist(task_id):
+        if task_id not in INDEX:
+            abort(404, message="Task {} does not exist".format(task_id))
     def get(self,task_id):
+        print("here it is ")
         abort_if_task_doesnt_exist(task_id)
+        
         if task_id=='task1':
             return("Please input a positive integer number with the format:</number>")
         else:
